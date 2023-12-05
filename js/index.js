21db057914c422644b5a21657309d136ba64df89
Armand Winant <145713109+BastienWinant@users.noreply.github.com>
2023-12-05 21:06:43 +0100

    copy to clipboard functionality

--- js/index.js
+++ js/index.js
@@ -4,10 +4,25 @@ const includeNumbersInput = document.querySelector("#include-numbers");
 const includeSpecCharsInput = document.querySelector("#include-special-chars");
 const passwordLengthInput = document.querySelector("#length");
 
+const passwordActions = document.querySelectorAll(".password-action");
+
 const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const specChars = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+let darkMode = false;
+const darkmodeToggle = document.querySelector(".darkmode-switch");
+
+function switchDarkmode() {
+  darkMode = !darkMode;
+
+  if (darkMode) {
+    darkmodeToggle.classList.add("medium-opacity");
+  } else {
+    darkmodeToggle.classList.remove("medium-opacity");
+  }
+}
 
 function generatePassword() {
   let characters = [...letters];
@@ -21,14 +36,12 @@ function generatePassword() {
   }
 
   let passwordLength = parseInt(passwordLengthInput.value);
-
-  if ((isNaN(passwordLength)) || (passwordLength <= 0) || (passwordLength > 20)) {
-    console.log("this will not work!");
-  }
-
   let password = "";
-  for (let i = 0; i < passwordLength; i++) {
-    password += characters[Math.floor(characters.length * Math.random())];
+
+  if (!((isNaN(passwordLength)) || (passwordLength <= 0) || (passwordLength > 20))) {
+    for (let i = 0; i < passwordLength; i++) {
+      password += characters[Math.floor(characters.length * Math.random())];
+    }
   }
 
   return password;
@@ -40,6 +53,9 @@ function newPassword() {
   }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Hello World");
-});
+function copyToClipboard(obj) {
+  const passwordField = obj.parentNode.firstElementChild;
+  navigator.clipboard.writeText(passwordField.textContent);
+}
+
+document.addEventListener("DOMContentLoaded", () => {});
