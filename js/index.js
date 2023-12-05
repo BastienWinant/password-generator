3127b2708c656ec6586fdb0f0e66eaa6614430b1
Armand Winant <145713109+BastienWinant@users.noreply.github.com>
2023-12-05 15:23:18 +0100

    add html elements and implement basic layouts

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const passwordContainers = document.querySelectorAll(".password-container");
+console.log(passwordContainers);
+
+function generatePasswords() {
+  console.log("Generating passwords...")
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Hello World");
+})
